e9beec63806c39da1340f5c5b209aae957aa9fe7
JustLinton <530065614@qq.com>
2023-08-03 12:00:00 +0800

    auto commit

--- beercraft/player.h
+++ beercraft/player.h
@@ -35,7 +35,7 @@ enum PlayerMovement
 class Player{
 public:
     Camera* camera;
-    glm::vec3 spawnPoint = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 spawnPoint = glm::vec3(0.0f, 0.0f, 0.0f);
     RenderContext* renderContext;
 
     Player(RenderContext *renderContext_) : renderContext(renderContext_)
@@ -46,7 +46,7 @@ public:
 
     void moveBackToSpawnPoint()
     {
-        camera->Position = glm::vec3(0.0f, 1.0f, 0.0f);
+        position = glm::vec3(0.0f, 0.0f, 0.0f);
     }
 
     void initCameraVision()
@@ -62,9 +62,11 @@ public:
     }
 
     void ProcessControl(PlayerMovement movement, float deltaTime);
+    void updatePosition(glm::vec3 pos);
 
-    private:
-        float speed = 5.5f; //default:2.5f mc:5.5f.
+private:
+    float speed = 5.5f; // default:2.5f mc:5.5f.
+    glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
 };
 
 void Player::ProcessControl(PlayerMovement movement, float deltaTime)
@@ -74,40 +76,47 @@ void Player::ProcessControl(PlayerMovement movement, float deltaTime)
         switch (movement)
         {
         case PLAYER_SPECT_FORWARD:
-            camera->Position += camera->Front * velocity;
+            updatePosition(camera->Position + camera->Front * velocity);
             break;
 
         case PLAYER_SPECT_BACK:
-            camera->Position -= camera->Front * velocity;
+            updatePosition(camera->Position - camera->Front * velocity);
             break;
 
         case PLAYER_SPECT_LEFT:
-            camera->Position -= camera->Right * velocity;
+            updatePosition(position - camera->Right * velocity);
             break;
 
         case PLAYER_SPECT_RIGHT:
-            camera->Position += camera->Right * velocity;
+            updatePosition(position + camera->Right * velocity);
             break;
 
         case PLAYER_FLY_UP:
-            camera->Position += glm::vec3(0.0f, 1.0f, 0.0f) * velocity;
+            updatePosition(position + glm::vec3(0.0f, 1.0f, 0.0f) * velocity);
             break;
 
         case PLAYER_FLY_DOWN:
-            camera->Position -= glm::vec3(0.0f, 1.0f, 0.0f) * velocity;
+            updatePosition(position - glm::vec3(0.0f, 1.0f, 0.0f) * velocity);
             break;
 
         case PLAYER_FLY_FORWARD:
-            camera->Position += glm::vec3(1.0f, 0.0f, 1.0f) * camera->Front * velocity;
+            updatePosition(position + glm::vec3(1.0f, 0.0f, 1.0f) * camera->Front * velocity);
             break;
 
         case PLAYER_FLY_BACK:
-            camera->Position -= glm::vec3(1.0f, 0.0f, 1.0f) * camera->Front * velocity;
+            updatePosition(position - glm::vec3(1.0f, 0.0f, 1.0f) * camera->Front * velocity);
             break;
 
         default : 
             break;
         }
+}
+
+void Player::updatePosition(glm::vec3 pos)
+{
+    position = pos;
+    // height of steve is 1.62.
+    camera->Position = position + glm::vec3(0.0f, 1.62f, 0.0f);
 }
 
 #endif
